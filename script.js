21c5b85a627cd6ba8bac2d23862b22b305c4f5a2
mohammadkhavarizadeh <mohammadkhavarizadeh.2001@gmail.com>
2026-06-19 18:06:22 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+﻿function loadUser() {
+
+    const user = QuestoriaStorage.getUser();
+
+    const level = document.getElementById("level");
+    const xp = document.getElementById("xp");
+    const coins = document.getElementById("coins");
+
+    if(level) level.textContent = user.level;
+    if(xp) xp.textContent = user.xp;
+    if(coins) coins.textContent = user.coins;
+
+}
+
+
+
+function startQuest() {
+
+    const quest = AIEngine.generateDailyQuest();
+
+    document.getElementById("questBox").innerHTML =
+    
+    <h3>Today's AI Quest</h3>
+    <p>${quest}</p>
+    <button onclick="completeQuest()">
+    Complete Quest
+    </button>
+    ;
+
+}
+
+
+
+function completeQuest(){
+
+    const quests = QuestEngine.getQuests();
+
+    const quest = quests[0];
+
+    QuestEngine.completeQuest(quest.id);
+
+    loadUser();
+
+    alert("Quest Completed!");
+
+}
+
+
+
+window.onload = function(){
+
+    loadUser();
+
+};
